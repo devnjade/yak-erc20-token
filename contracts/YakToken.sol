67b55ee413f5b8a023a17d845dabf1e449b93ee3
Adebayo Temitope <adecancode@gmail.com>
2022-07-18 20:13:09 +0100

    update

--- contracts/YakToken.sol
+++ contracts/YakToken.sol
@@ -7,5 +7,69 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YakToken is Ownable, ERC20, ReentrancyGuard {
-    constructor(uint256 initialSupply) ERC20("Yak", "YAK") {}
+    uint256 public totalTokenSupply;
+    uint256 public claimAmount;
+
+    uint256 public totalAddressesAllowed;
+
+    mapping(address => bool) addresses;
+    mapping(address => uint256) amountOfClaims;
+
+    constructor() ERC20("Yak", "YAK") {
+        totalAddressesAllowed = 0;
+    }
+
+    function totalClaimed() public view returns (uint256) {
+        return totalTokenSupply;
+    }
+
+    function mint(address to) public onlyOwner {
+        require(verifyAddress(to), "Address not allowed to claim token");
+        require(amountOfClaims[to] == 0, "Address already claimed");
+
+        _mint(to, claimAmount);
+        amountOfClaims[to]--;
+        totalTokenSupply += claimAmount;
+    }
+
+    function updateClaimAmount(uint256 amount) public onlyOwner {
+        claimAmount = amount;
+    }
+
+    function addClaimAddress(address addr, uint256 claim) public onlyOwner {
+        require(!addresses[addr], "Address already added");
+        addresses[addr] = true;
+        amountOfClaims[addr] = claim;
+        totalAddressesAllowed++;
+    }
+
+    function addClaimAddresses(address[] memory addr, uint256 claim)
+        public
+        onlyOwner
+    {
+        for (uint256 i = 0; i < addr.length; i++) {
+            addClaimAddress(addr[i], claim);
+        }
+    }
+
+    function removeClaimAddress(address addr) public onlyOwner {
+        require(addresses[addr], "Address not added");
+        addresses[addr] = false;
+        amountOfClaims[addr] = 0;
+        totalAddressesAllowed--;
+    }
+
+    function removeClaimAddresses(address[] memory addr) public onlyOwner {
+        for (uint256 i = 0; i < addr.length; i++) {
+            removeClaimAddress(addr[i]);
+        }
+    }
+
+    function verifyAddress(address addr) public view returns (bool) {
+        return addresses[addr];
+    }
+
+    function checkAmountOfClaims(address addr) public view returns (uint256) {
+        return amountOfClaims[addr];
+    }
 }
